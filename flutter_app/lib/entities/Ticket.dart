897f8875448c27b0db325269f2c1e2e4ca1a67ea
Jeff Davies <297151+jdavies@users.noreply.github.com>
2021-07-22 11:07:35 -0700

    Added to the project

---
+++ flutter_app/lib/entities/Ticket.dart
@@ -0,0 +1,12 @@
+class Ticket {
+  late String id;
+  late String departure_gate;
+  late String departure_time;
+  late String destination_city;
+  late String origin_city;
+  late String checkin_time;
+  late String flight_id;
+  late String passenger_id;
+  late String passenger_name;
+  late String carousel;
+}
